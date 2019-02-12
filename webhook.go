93ca6104929e1e11cb4a05b8cccf6f73f8d9cc00
Scott Stensland <scottstensland@gmail.com>
2019-02-12 07:57:59 -0500

    modified:   webhook.go

--- webhook.go
+++ webhook.go
@@ -280,17 +280,21 @@ func hookHandler(w http.ResponseWriter, r *http.Request) {
 			}
 		}
 
-		log.Printf("\n\n headers ")
-		log.Printf(headers)
+		// log.Printf("\n\n headers ")
+		// log.Printf(headers)
+		log.Printf("\n\n headers [%s]\n", headers)
 
-		log.Printf("\n\n query ")
-		log.Printf(query)
+		// log.Printf("\n\n query ")
+		// log.Printf(query)
+		log.Printf("\n\n query [%s]\n", query)
 
-		log.Printf("\n\n payload ")
-		log.Printf(payload)
+		// log.Printf("\n\n payload ")
+		// log.Printf(payload)
+		log.Printf("\n\n payload [%s]\n", payload)
 
-		log.Printf("\n\n body ")
-		log.Printf(body)
+		// log.Printf("\n\n body ")
+		// log.Printf(body)
+		log.Printf("\n\n body [%s]\n", string(body[:]))
 
 		if ok {
 			log.Printf("[%s] %s hook triggered successfully\n", rid, matchedHook.ID)
